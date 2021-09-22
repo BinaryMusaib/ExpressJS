d727a6c7560610cd11d93f37939b93ab2e772220
Musaib Bilal <hotmusaib@gmail.com>
2021-09-22 10:01:04 +0530

    Part2: Middleware

---
+++ Express/2-Middleware.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+
+app.get('/', logger, (req, res) => {
+    res.send('Home Page')
+})
+
+app.get('/about', logger, (req, res) => {
+    res.send('About Page')
+})
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000...');
+})
